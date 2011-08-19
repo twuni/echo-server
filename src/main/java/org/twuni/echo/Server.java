4f45a4e1c139a65e0002c3add6995dd22379b621
Leo Accend <github.aeonic@spamgourmet.com>
2011-08-18 23:28:32 -0700

    Externalizing configuration into echo.properties.

--- src/main/java/org/twuni/echo/Server.java
+++ src/main/java/org/twuni/echo/Server.java
@@ -1,6 +1,8 @@
 package org.twuni.echo;
 
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.twuni.common.config.Configuration;
+import org.twuni.common.config.PropertiesConfiguration;
 import org.twuni.common.net.http.Method;
 import org.twuni.common.net.http.responder.ExceptionHandler;
 import org.twuni.common.net.http.responder.RequestMapping;
@@ -14,16 +16,18 @@ public class Server {
 
 	public static void main( String [] args ) {
 
+		Configuration configuration = PropertiesConfiguration.load( "echo" );
+
 		StringBuilder buffer = new StringBuilder();
-		Treasury treasury = new TreasuryClient( new DefaultHttpClient(), "http://localhost:8080" );
-		
+		Treasury treasury = new TreasuryClient( new DefaultHttpClient(), configuration.getString( "treasury.url", "http://localhost:8080" ) );
+
 		RequestMapping mapping = new RequestMapping();
 
 		mapping.map( Method.GET, "/", new FormResponder() );
 		mapping.map( Method.POST, "/earn", new EarnResponder( treasury, buffer ) );
 		mapping.map( Method.POST, "/redeem", new RedeemResponder( treasury, buffer ) );
 
-		new org.twuni.common.net.http.Server( 8081, new ExceptionHandler( mapping ) ).start();
+		new org.twuni.common.net.http.Server( configuration.getInt( "server.port", 80 ), new ExceptionHandler( mapping ) ).start();
 
 	}
 
